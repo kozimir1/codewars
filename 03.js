b22be7a53ef5cdf94393940d10987b5fd22ce7d4
DM <bezrukovf@gmail.com>
2023-02-15 21:08:43 +0400

    commit-3 ex-3

---
+++ 03.js
@@ -0,0 +1,8 @@
+function squareSum(numbers) {
+    let result = 0
+    for (let i = 0; i < numbers.length; i++) {
+        result += Math.pow(numbers[i], 2)
+    }
+    return result
+}
+squareSum([1, 2])
